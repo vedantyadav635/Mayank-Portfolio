2e327081d8d4eed50c13efb2d0917d9e4e7ba7a6
Vedant Yadav <151537657+vedantyadav635@users.noreply.github.com>
2025-02-07 12:53:13 +0530

    Add files via upload

---
+++ src/data/WorkData.js
@@ -0,0 +1,54 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Music Liquid Logo Animation",
+        description:"It is a dynamic liquid-style logo animation synchronized to music, designed in Adobe After Effects for a visually engaging and rhythmic brand reveal.",
+        tags:[],
+        
+        demo:"https://youtu.be/ZiRiN9c01Jg?si=PdOjTdtSVKCCxlg6",
+        github:""
+    },
+    {
+        id:2,
+        name:"Nike Shoes Animation",
+        description:"It is a high-energy Nike shoe animation video, designed in Adobe After Effects, showcasing dynamic motion, sleek transitions, and powerful visual effects.",
+        tags:[],
+        
+        demo:"https://youtu.be/0tiR1FLCJWA?si=QaVQSQ30aXT9PwGQ",
+        github:""
+        
+    },
+    {
+        id:3,
+        name:"Time to Travel explainer video",
+        description:"It is a captivating Time to Travel explainer video, featuring smooth animations and engaging visuals to inspire adventure and exploration..",
+        tags:[],
+        
+        demo:"https://youtu.be/1qqg0QftjTk?si=ELgioEyzPGaOgvmi",
+        github:""
+    },
+    {
+        id:4,
+        name:"Headphone AD animation",
+        description:"It is a sleek and dynamic headphone ad animation, showcasing immersive sound quality with smooth transitions and vibrant visuals.",
+        tags:[],
+        
+        demo:"https://youtu.be/wCKh-mCyXAk?si=V0SSqP140p_m08rJ",
+        github:""
+       
+    },
+    {
+        id:5,
+        name:"Explainer AD porter logistics",
+        description:"It is an engaging explainer ad animation for Porter Logistic Service, , highlighting seamless delivery solutions with smooth motion graphics and clear storytelling.",
+        tags:[],
+        
+        demo:"https://youtu.be/wCKh-mCyXAk?si=V0SSqP140p_m08rJ",
+        github:""
+       
+    }
+    
+  
+] 
---
+++ src/subComponents/SocialIcons.js
@@ -0,0 +1,122 @@
+import { motion } from "framer-motion";
+import React from "react";
+// import { NavLink } from 'react-router-dom'
+import styled from "styled-components";
+import { Facebook, Github, Twitter, YouTube } from "../components/AllSvgs";
+import { DarkTheme } from "../components/Themes";
+
+const Icons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: fixed;
+  bottom: 0;
+  left: 2rem;
+
+  z-index: 3;
+
+  & > *:not(:last-child) {
+    margin: 0.5rem 0;
+  }
+`;
+
+const Line = styled(motion.span)`
+  width: 2px;
+  height: 8rem;
+  background-color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+`;
+
+const SocialIcons = (props) => {
+  return (
+    <Icons>
+      <motion.div
+        initial={{scale:0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          href={"https://www.behance.net/mayankvpathak"}
+        >
+          <Github
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+      <motion.div
+        initial={{scale:0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.2 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          href={"https://x.com/xomayank023"}
+        >
+          <Twitter
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+      <motion.div
+        initial={{scale:0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.4 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          href={"https://www.instagram.com/xo_mayank023/?next=%2F&hl=en"}
+        >
+          
+          <Facebook
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+      <motion.div
+        initial={{scale:0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.6 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          href={"https://www.linkedin.com/in/xomayank023/"}
+        >
+          <YouTube
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+
+      <Line
+        color={props.theme}
+        initial={{
+          height: 0,
+        }}
+        animate={{
+          height: "8rem",
+        }}
+        transition={{
+          type: "spring",
+          duration: 1,
+          delay: 0.8,
+        }}
+      />
+    </Icons>
+  );
+};
+
+export default SocialIcons;
